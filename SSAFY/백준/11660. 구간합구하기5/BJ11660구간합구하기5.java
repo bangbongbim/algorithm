331114ffc7d69a6d728fd3e38d3dc90c1bf9a407
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2022-08-04 01:29:19 +0900

    BJ11660구간합구하기5

--- SSAFY/백준/11660. 구간합구하기5/BJ11660구간합구하기5.java
+++ SSAFY/백준/11660. 구간합구하기5/BJ11660구간합구하기5.java
@@ -1,13 +1,11 @@
-package basic;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BJ11660구간합구하기5 {
+public class BJ11660구간합구하기 {
 	static int N, M;
 	static int arr[][];
-	static int s1, s2, e1, e2;
+	static int x1, y1, x2, y2;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,19 +18,24 @@ public class BJ11660구간합구하기5 {
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
-				arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
+				arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+				// 구간합 더하고 중복되는 부분 빼줌
 			}
 		}
+		// ex) (3,4) => (1,1) ~ (3,4)까지의 합
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			s1 = Integer.parseInt(st.nextToken());
-			s2 = Integer.parseInt(st.nextToken());
-			e1 = Integer.parseInt(st.nextToken());
-			e2 = Integer.parseInt(st.nextToken());
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
 
-			System.out.println(arr[e1][e2] - arr[s1][s2]);
+			sb.append(arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1-1][y1 - 1]).append("\n");
+			// ex) (2,2) (3,4) 일 경우   (1,1)~(3,4)까지의 합  - (1,1) ~(1,4)까지의 합  - (1,1) ~ (1,4)까지의 합 + 중복된 부분
 		}
+		System.out.println(sb.toString());
 
 	}
 
